81ce3f7e1d751b8fac684a5a25321f3036d8ea7f
Connor Sughrue <cpsughrue@gmail.com>
2021-11-13 00:20:27 +0100

    made changes

--- data_processing.py
+++ data_processing.py
@@ -3,83 +3,82 @@ import sqlite3
 import os
 import re
 
-def get_data():
+def get_shooting_data(path = "data.csv"):
     '''
     produces csv file "data.csv" with latest version of washington post database
     '''
     dirty = "dirty_data.csv"
-    clean = "data.csv"
 
     url = "https://raw.githubusercontent.com/washingtonpost/data-police-shootings/master/fatal-police-shootings-data.csv"
-    pd.read_csv(url, index_col="id").to_csv(dirty)
+    pd.read_csv(url, index_col = "id").to_csv(dirty)
 
-    file = open(clean, 'wt')
-    with open("dirty_data.csv", 'rt') as f:
+    file = open(path, 'wt')
+    # this is where all data cleaning takes place
+    with open(dirty, 'rt') as f:
         for line in f:
-            line = re.sub(' +', ' ', line)
+            line = re.sub(' +', ' ', line) # removes extra spaces
             file.write(line)
     file.close()
     os.remove(dirty)
 
 
-def edit_features():
+def edit_features(path = "data.csv"):
     '''
     function for all feature engineering prior to being loaded into a relational database
     '''
-    path = "data.csv"
-    data = pd.read_csv(path, index_col="id")
-    data[['year', 'month', 'day']] = data['date'].str.split('-', expand=True)
+    data = pd.read_csv(path, index_col = "id")
+    data[['year', 'month', 'day']] = data['date'].str.split('-', expand = True)
     data.to_csv(path)
 
 
-def load_db():
+def load_db(path = 'data.csv', database = "shooting.db"):
     '''
     loads data in csv file into a relational database for easy querying
     '''
-    data = pd.read_csv('data.csv')
-
-    database = "shooting.db"
+    # because the dataset is so small it is significantly 
+    # easier to create a new database each time the data is updated
+    # then to only add the new rows
     if os.path.isfile(database):
         os.remove(database)
 
-    conn = sqlite3.connect("shooting.db")
+    conn = sqlite3.connect(database)
     curs = conn.cursor()
-
-    sql = """CREATE TABLE tShooting (
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            date TEXT,
-            manner_of_death TEXT,
-            armed TEXT,
-            age INTEGER,
-            gender TEXT,
-            race TEXT,
-            city TEXT,
-            state TEXT,
-            signs_of_mental_illness TEXT,
-            threat_level TEXT,
-            flee TEXT,
-            body_camera TEXT,
-            longitude REAL,
-            latitude REAL,
-            is_geocoding_exact TEXT,
-            year INTEGER,
-            month INTEGER,
-            day INTEGER);"""
-
+    sql = """CREATE TABLE tShooting (id INTEGER PRIMARY KEY,
+                                     name TEXT,
+                                     date TEXT,
+                                     manner_of_death TEXT,
+                                     armed TEXT,
+                                     age INTEGER,
+                                     gender TEXT,
+                                     race TEXT,
+                                     city TEXT,
+                                     state TEXT,
+                                     signs_of_mental_illness TEXT,
+                                     threat_level TEXT,
+                                     flee TEXT,
+                                     body_camera TEXT,
+                                     longitude REAL,
+                                     latitude REAL,
+                                     is_geocoding_exact TEXT,
+                                     year INTEGER,
+                                     month INTEGER,
+                                     day INTEGER);"""
     curs.execute(sql)
-    sql = "INSERT INTO tShooting VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);"
+    
+    data = pd.read_csv(path)
+    insert_row = "INSERT INTO tShooting VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);"
     for row in data.values:
-        curs.execute(sql, tuple(row))
+        curs.execute(insert_row, tuple(row))
+        
     conn.commit()
     conn.close()
 
 
-def view_schema():
+def view_schema(database = "shooting.db"):
     '''
     prints out database schema
     '''
-    conn = sqlite3.connect("shooting.db")
+    conn = sqlite3.connect(database)
     x = pd.read_sql(""" SELECT name 
                         FROM sqlite_master
                         WHERE type = 'table'
@@ -90,5 +89,32 @@ def view_schema():
         print(table)
         print(pd.read_sql(sql, conn))
         print('\n')
-
     conn.close()
+
+
+def view_tShooting(database = "shooting.db"):
+    '''
+    prints first five rows of tShooting to help confirm everything was loaded correctly
+    '''
+    conn = sqlite3.connect(database)
+    x = pd.read_sql(""" SELECT * 
+                        FROM tShooting;""", conn)
+    print(x.head())
+
+
+def main():
+
+    path = "data.csv"
+    database = "shooting.db"
+
+    get_shooting_data(path)
+    edit_features(path)
+    load_db(path, database)
+    view_schema(database)
+    view_tShooting(database)
+
+
+if __name__ == "__main__":
+    main()
+
+    
